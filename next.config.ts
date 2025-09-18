f0ed44ecef13e0cbf9b072f84270621e1dcc0a34
snirmalkumar06 <s.nirmalkumar@enspirit.co>
2025-09-18 19:30:29 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+export default bundleAnalyzer(nextConfig);
